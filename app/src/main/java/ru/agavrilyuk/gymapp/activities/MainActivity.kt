e397c6453507a81def5f485401a58166dc69aa4f
SlyRedFox <one1970zero@mail.ru>
2023-10-12 21:37:21 +0300

    Add binding

--- app/src/main/java/ru/agavrilyuk/gymapp/activities/MainActivity.kt
+++ app/src/main/java/ru/agavrilyuk/gymapp/activities/MainActivity.kt
@@ -3,13 +3,17 @@ package ru.agavrilyuk.gymapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.agavrilyuk.gymapp.R
+import ru.agavrilyuk.gymapp.databinding.ActivityMainBinding
 import ru.agavrilyuk.gymapp.fragments.DaysFragment
 import ru.agavrilyuk.gymapp.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         FragmentManager.setFragment(DaysFragment.newInstance(), this)
+
     }
 }
